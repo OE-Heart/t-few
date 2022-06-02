b0ef55840a3d869e7eee301de044b3edabb3178e
OE-Heart <leoouyixin@126.com>
2022-06-02 17:22:15 +0800

    Add data_analysis

--- bin/start.sh
+++ bin/start.sh
@@ -4,3 +4,4 @@ export PYTHONPATH=$NICL_ROOT:$PYTHONPATH
 export OUTPUT_PATH=$NICL_ROOT/exp_out
 export CONFIG_PATH=$NICL_ROOT/configs
 export HF_HOME=/home/ouyixin/.cache/huggingface
+export CUDA_VISIBLE_DEVICES=0
